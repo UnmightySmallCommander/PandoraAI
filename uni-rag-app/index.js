f14c6fcd890dff91ee52537e2a042a32b1aa7ab2
UnmightySmallCommander <chatdanai.padee@gmail.com>
2026-04-01 19:58:35 +0700

    Update index.js

--- uni-rag-app/index.js
+++ uni-rag-app/index.js
@@ -198,6 +198,6 @@ Rules you must follow:
 });
  
 // --- Start server ---
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running on port", process.env.PORT || 3000);
 });
